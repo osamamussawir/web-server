b45caba7c271b3af043c80d02978f409cdbe78a8
Kashif Ali Siddiqui <kashif.ali.siddiqui@macys.com>
2018-10-01 18:24:06 +0500

    update port for heroku

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ var app = express();
 
 var middleware = require('./middleware');
 
+//heroku set this process.env.Port
+var PORT =process.env.PORT || 3000;
+
 // var middleware = {
 
 //     requiredAuthentication: function(req , res , next) {
@@ -40,6 +43,6 @@ res.send('About us!');
 
 app.use(express.static(__dirname + '/public'));
 
-app.listen(3000,function(){
-    console.log('Express server has been started');
+app.listen(PORT,function(){
+    console.log('Express server has been started on: ' + PORT);
 });
